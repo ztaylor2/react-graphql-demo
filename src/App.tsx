fa3c848508bdd48c6ba92e82a6069d36e8a0332c
ztaylor2 <zacharymtaylor3@gmail.com>
2019-11-10 12:46:58 -0800

    init project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const EXCHANGE_RATES = gql`
+	{
+		rates(currency: "USD") {
+			currency
+			rate
+		}
+	}
+`;
+
+interface Data {
+    currency: string;
+    rate: number;
+}
+
+interface CurrencyData {
+    rates: Data[];
+}
+
+const ExchangeRates: React.FC = () => {
+	const { loading, error, data } = useQuery<CurrencyData, Data>(EXCHANGE_RATES);
+
+	if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+	return (
+		<div>
+			{data && data.rates.map(({ currency, rate }) => (
+				<div key={currency}>
+					<p>
+						{currency}: {rate}
+					</p>
+				</div>
+			))}
+		</div>
+	)
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+                <ExchangeRates />
+			</header>
+		</div>
+	);
 }
 
 export default App;
